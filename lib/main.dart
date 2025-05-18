df258123d213f59277629701a0ee67692bcd6036
BanulaJayaweera <banujayaweera@gmail.com>
2025-05-18 18:20:52 +0530

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'pages/page1.dart'; // Import the WelcomeScreen
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
+  await Firebase.initializeApp(); // Initialize Firebase
   runApp(const InfantFuelApp());
 }
 
